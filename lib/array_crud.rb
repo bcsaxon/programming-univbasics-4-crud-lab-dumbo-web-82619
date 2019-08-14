02ec8b91069441ec1ec7eaaea4e9228c800d3fbe
Brett Saxon <brettcsaxon@gmail.com>
2019-08-14 14:05:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  family_array = ["eli","brett","bob","marietta"]
+  
 end
 
 def retrieve_first_element_from_array(array)
