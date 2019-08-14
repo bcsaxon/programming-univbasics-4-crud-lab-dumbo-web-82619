9ff00bb092066407586ab125e28b3715f0254453
Brett Saxon <brettcsaxon@gmail.com>
2019-08-14 13:52:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   family_array = ["eli","brett","bob","marietta"]
-  arrays! = family_array.pop
+  "arrays!"" = family_array.pop
 end
 
 def remove_element_from_start_of_array(array)
